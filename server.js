805433b650cea000b8d714e0ac353f1ed1480b64
Parul <parul.srivastava.2004@gmail.com>
2023-10-12 00:10:29 +0530

    editing

--- server.js
+++ server.js
@@ -1,62 +1,27 @@
-const path = require('path');
-const http = require('http');
+require('dotenv').config();
+const { Server } = require('http'); // Use Server from http module
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const {userJoin, getCurrentUser,userLeave,getRoomUsers} = require('./utils/users');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+
 const app = express();
-const server = http.createServer(app);
+
+// Create an HTTP server from the express app
+const server = new Server(app);
 const io = socketio(server);
-//Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Set static folder
+app.use(express.static('public')); // Vercel will automatically serve files from 'public'
 
 const botName = 'ChatCord Bot';
 
-//run when client connects
-io.on('connection', socket => {
-    socket.on('joinRoom', ({ username, room }) => {
-        const user = userJoin(socket.id, username, room);
+// Export the Express app as a function
+module.exports = app;
 
-        socket.join(user.room);
+// Code for your socket.io functionality remains unchanged
 
-        //welcome current user
-        socket.emit('message', formatMessage(botName, 'Welcome to our chatroom!'));
+const port = process.env.PORT || 3000;
 
-        //broadcast when a user connects
-        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
-
-        //send users and room info
-        io.to(user.room).emit('roomUsers', {
-            room: user.room,
-            users: getRoomUsers(user.room)    
-        });
-    });
-
-    console.log('New WS Connection...');
-
-    //listen for chatMessage
-    socket.on('chatMessage', msg => {
-        const user = getCurrentUser(socket.id);
-
-        io.to(user.room).emit('message',formatMessage(user.username,msg));
-    });
-
-    //runs when client disconnects
-    socket.on('disconnect', () => {
-        const user = userLeave(socket.id);
-
-        if(user){
-            io.to(user.room).emit('message', formatMessage(botName,`${user.username} has left the chat`));
-
-            //send users and room info
-            io.to(user.room).emit('roomUsers', {
-                room: user.room,
-                users: getRoomUsers(user.room)    
-            });
-        }    
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// We don't need the server to listen here, as Vercel handles this for you
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
